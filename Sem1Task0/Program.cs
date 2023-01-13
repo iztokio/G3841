16dfefe9c48f98820262151bc33384932e505ab9
Artem <iztokio1984@mail.ru>
2023-01-13 22:10:14 +0300

    add solution for task 0,1,3,5,7

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача №0
+//Пишем программу , которая умножает число само на себя
+
+//Считаем данные с консоли
+string? inputNum = Console.ReadLine();
+
+//СПроверяем чтобы данные были не пустыми
+if(inputNum!=null)
+{
+    //Парсим введенное число
+    int number = int.Parse(inputNum);
+    //Находим квадрат числа
+    //int result = number*number;
+    int result = (int)Math.Pow(number,2);
+
+    //Выводим данные в консоль
+    Console.WriteLine(result);
+
+}
