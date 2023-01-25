5898c2ba2620616761faa94f5deab4ffbc2477b6
Artem <iztokio1984@mail.ru>
2023-01-25 22:24:56 +0300

    6done

---
+++ Sem1Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿string? inputNum = Console.ReadLine();
+if(inputNum!=null)
+{
+    int number = int.Parse(inputNum);
+    if (number % 2 == 0)
+    {
+        Console.WriteLine(number + " является четным числом");
+    }
+    else
+    {
+        Console.WriteLine(number + " является НЕ четным числом");
+    }
+}
