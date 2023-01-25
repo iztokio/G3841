ead760e6e5080e3a561ab0e41777556e11162ad3
Artem <iztokio1984@mail.ru>
2023-01-25 21:49:04 +0300

    2done

--- Sem1Task2/Program.cs
+++ Sem1Task2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string? inputNum1 = Console.ReadLine();
+string? inputNum2 = Console.ReadLine();
+if(inputNum1!=null && inputNum2!=null)
+{
+    int number1 = int.Parse(inputNum1);
+    int number2 = int.Parse(inputNum2);
+    if (number1 > number2)
+    {
+        Console.WriteLine(number1);
+    }
+    else
+    {
+        Console.Write(number2);
+    }
+}
