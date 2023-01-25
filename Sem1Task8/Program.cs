7fc0dcf9596b1e359e8706c8ad411360f235f390
Artem <iztokio1984@mail.ru>
2023-01-25 22:49:32 +0300

    8done

---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,15 @@
+﻿string? inputNum = Console.ReadLine();
+if(inputNum!=null)
+{
+    int number = int.Parse(inputNum);
+    int count = 1;
+    while(count < number + 1)
+    {
+        if(count % 2 == 0)
+        {
+            Console.Write(count + ", ");
+        }
+        count = count + 1;
+    }
+    
+}
