88d2bc9694bfcd9c39b097fd31e5e925b58e8975
Artem <iztokio1984@mail.ru>
2023-01-28 05:16:12 +0300

    13done

--- Sem2Task13/Program.cs
+++ Sem2Task13/Program.cs
@@ -1,15 +1,10 @@
-﻿
-
-int num = int.Parse(Console.ReadLine()??"0");
-Console.WriteLine(num);
-//string value = num.ToString();
-char[] digit = num.ToString().ToCharArray();
-Console.WriteLine(digit);
-// if(digit.Length > 3)
-// {
-//     Console.WriteLine(digit[2]);
-// }
-// else
-// {
-//     Console.WriteLine("Третий цифры нет");
-// }    
+﻿int num1 = int.Parse(Console.ReadLine()??"0");
+char[] digit = num1.ToString().ToCharArray();
+if(digit.Length >= 3)
+{
+    Console.WriteLine(digit[2]);
+}
+else
+{
+    Console.WriteLine("Третий цифры нет");
+}    
