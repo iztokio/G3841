194477975a97d387325027b1f72faa86fa4131b9
Artem <iztokio1984@mail.ru>
2023-01-29 21:44:55 +0300

    19done

---
+++ Sem3Task19/Program.cs
@@ -0,0 +1,25 @@
+﻿int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+bool PalinTest(int n)
+{
+    bool res = true;
+    int d1 = n / 10000;
+    int d2 = (n / 1000) % 10;
+    int d4 = (n / 10) % 10;
+    int d5 = n % 10;
+    res = ((d1 == d5) && (d2 == d4)) ? true : false;
+    return res;
+}
+
+void PrintData(string msg, bool res)
+{
+    Console.WriteLine(msg + res);
+}
+
+int n = ReadData("Введите число");
+bool res = PalinTest(n);
+PrintData($"Утверждение что число {n} палиндром, является: ", res);
