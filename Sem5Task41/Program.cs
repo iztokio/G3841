c2ecf0dce9b4496a34c74835326faf3d4a8671a6
Artem <iztokio1984@mail.ru>
2023-02-10 02:26:38 +0300

    43done

---
+++ Sem5Task41/Program.cs
@@ -0,0 +1,62 @@
+﻿// №41 Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл
+// пользователь.
+// * Пользователь вводит число нажатий, затем программа следит за нажатиями и
+// выдает сколько чисел больше 0 было введено.
+
+
+// Метод ввода количества чисел
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод, генерирующий массив
+int[] GenArrayDigit(int len)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите число № {i+1}: ");
+        arr[i] = int.Parse(Console.ReadLine() ?? "0");
+        Console.WriteLine();
+    }
+    return arr;
+}
+
+//Метод, выводящий на печать массив
+void Print1DArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+// Метод подсчета положительных чисел
+int GenResalt(int []arr)
+{
+    int Count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            Count = Count + 1;
+        }
+    }
+    return Count;
+}
+
+// Метод вывода результата
+void PrintData(string res, int value)
+{
+    Console.WriteLine(res + value);
+}
+
+int Digit = ReadData("Введите количество чисел: ");
+int [] ArrayDigit = GenArrayDigit(Digit);
+Print1DArr(ArrayDigit);
+int value = GenResalt(ArrayDigit);
+PrintData("Количество чисел больше 0: ", value);
---
+++ Sem5Task43/Program.cs
@@ -0,0 +1,34 @@
+﻿// №43 Напишите программу, которая найдёт точку пересечения двух прямых, заданных
+// уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются
+// пользователем.
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+void PrintData(string prefix, string data)
+{
+    Console.WriteLine(prefix + data);
+}
+
+double[] PointFind(int k1, int b1, int k2, int b2)
+{
+    double[] outArr = new double[2];
+    double x = (double)(b2 - b1) / (double)(k1 - k2);
+    double y = k1 * x + b1;
+    outArr[0] = x;
+    outArr[1] = y;
+    return outArr;
+}
+
+int k1 = ReadData("Введите точку k1: ");
+int b1 = ReadData("Введите точку b1: ");
+int k2 = ReadData("Введите точку k2: ");
+int b2 = ReadData("Введите точку b2: ");
+
+double[] Point = PointFind(k1, b1, k2, b2);
+
+PrintData("Точка пересечения двух линий: ", $"({Point[0]};{Point[1]})");
